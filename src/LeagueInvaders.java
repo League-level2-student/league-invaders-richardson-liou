b93f5651358ffcbe29859bfeabb74f45dab6fee0
league <league@imac-14.attlocal.net>
2022-08-27 11:28:19 -0700

    League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel panel = new GamePanel();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	LeagueInvaders(){
+		frame = new JFrame();
+	}
+	public void setup() {
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	public static void main(String[] args) {
+		LeagueInvaders invade = new LeagueInvaders();
+		invade.setup();
+		
+	}
 }
